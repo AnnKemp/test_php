cf8ec39be473606573d75edf75928863e10b5131
AnnKemp <ann.kemp@scarlet.be>
2020-02-06 11:40:48 +0100

    evolutions added the small pokemon and his name to it

--- index.php
+++ index.php
@@ -13,15 +13,13 @@ if (!empty($_GET)) {
   $get = file_get_contents("https://pokeapi.co/api/v2/pokemon/".$pokeName);
     $response = json_decode($get, true); //because of true, it's in an array
    //var_dump($response);
-    // get and show name
+    // get and show the name as a kind of title
     echo '<H1>'.($response['forms'][0]['name']).'</H1>';
     // put it in a var
     $namePoke=($response['forms'][0]['name']);
     // get and show image
-    if($namePoke=="gyarados"){
-      //  top: 109px;
-    //margin-left:-290p
-        echo '<img src="'.($response['sprites']['front_default']).'" width="130" id="bigPokemon" style="" />';
+    if(($namePoke=="gyarados")||($namePoke=="venusaur")||($namePoke=="charmeleon")){ // fixing an esthetic problem some pokemons are too big
+        echo '<img src="'.($response['sprites']['front_default']).'" width="130" id="bigPokemon" style="top: 150px; margin-left:-240px" />';
     }else{
         echo '<img src="'.($response['sprites']['front_default']).'" width="230" id="bigPokemon" />';
     }
@@ -35,6 +33,7 @@ if (!empty($_GET)) {
     for ($x=0;$x<4;$x++){
         $move=($response['moves'][$random_keys[$x]]['move']['name']);
         if (empty($move)) {
+            $move=""; // dit was om die foutmeldingen te doen stoppen maar dat doen ze niet maar het werkt dus ik zet ze gewoon af.
             echo '<li></li>';
         }else {
             echo '<li>' . $move . '</li>';
@@ -42,46 +41,37 @@ if (!empty($_GET)) {
     }
     echo "</ul>";
     echo "</div>";
-
+    // get the string with the id at the end
   $id_string=($response['forms'][0]['url']);
+  // cut the id off with substring
   $id=substr($id_string,-2,-1);
 
   echo '<p id="id">'.$id."</p>";
 
-   // $get_2= file_get_contents("https://pokeapi.co/api/v2/pokemon-species/".$id);
-   // $get_2= file_get_contents("https://pokeapi.co/api/v2/pokemon/type/3/".$id);
-    //$response_2 = json_decode($get_2, true); //because of true, it's in an array
-    //var_dump($response_2);
+  // search into the spieces by poke-name
+$get_next= file_get_contents("https://pokeapi.co/api/v2/pokemon-species/".$namePoke);
+$response_next = json_decode($get_next, true);    // because of true, it's in an array
+//var_dump($response_next);
 
-  /*  async function getPrevo() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + input.value.toLowerCase() + "");
-    // let response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value.toLowerCase()}`);
-    let evolutionData = await response.json();
+   $checkIf_data=$response_next['evolves_from_species']['name'];
 
-    console.log(evolutionData);
-    if (evolutionData.evolves_from_species == null) {
-        document.getElementById('prevEvolution').innerHTML = "";
-        evoImage.setAttribute("src", "")
+   if(empty($checkIf_data)) {
+        echo '<p id="smallOne_name"></p>';
     } else {
-        const preName = evolutionData.evolves_from_species.name;
-        document.getElementById('prevEvolution').innerHTML = "Previous Evolution: " + preName;
-        preForm(preName);
+       // get in this get-date name of the small one
+       $evolves_name=$response_next['evolves_from_species']['name'];
+       echo '<p id="smallOne_name">'.$evolves_name.'</p>';
+
+       // do again a get with this name to get the sprite/picture of the small one
+       $get_smallOne= file_get_contents("https://pokeapi.co/api/v2/pokemon/".$evolves_name);
+       $response_smallOne = json_decode($get_smallOne, true);    // because of true, it's in an array
+//var_dump($response_smallOne);
+
+       // put the link into an img src to show it
+       echo '<img src="'.($response_smallOne['sprites']['front_default']).'" width="230" id="smallPokemon" />';
     }
-
-    nextEvo = evolutionData.evolution_chain.url;
-    getNext(nextEvo);
-}
-
-async function preForm(prevolution) {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${prevolution}`);
-    let preData = await response.json();
-    console.log(preData);
-    let pokemonSprite = preData.sprites.front_default;
-
-    evoImage.setAttribute("src", pokemonSprite);
-    // co */
 }else{
-    echo "didn't receive any data from the form";
+    echo "<p id='noData_error'>Please fill in a name and click on the round blue 'search' button</p>";
 }
 ?>
 <html>
